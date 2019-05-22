e4f7efda96942d9c804cf3bf7e6d093f93e714ad
Alexander Poschenrieder <aposchenrieder@atixlabs.com>
2019-05-22 13:51:40 -0300

    Agrego fixes al login y su integracion con la app react

--- grupo3-backend/grupo3-repository/src/main/java/utn/tacs/grupo3/repository/UserRepository.java
+++ grupo3-backend/grupo3-repository/src/main/java/utn/tacs/grupo3/repository/UserRepository.java
@@ -47,6 +47,19 @@ public class UserRepository {
                 .orElseThrow(() -> new ExceptionbyUserNotFound(name));
     }
 
+    public List<User> usersByUsername(String nombre) {
+        return users.stream().
+                filter(user -> user.getUsername().equalsIgnoreCase(nombre)).
+                collect(Collectors.toList());
+    }
+
+
+    public User userByUsername(String name) throws ExceptionbyResourceNotFound {
+        return usersByUsername(name)
+                .stream().findFirst()
+                .orElseThrow(() -> new ExceptionbyUserNotFound(name));
+    }
+
     public void createUser(User user) {
         users.add(user);
     }
--- grupo3-frontend-react/grupo3-react-application/src/views/login/user/login_user.jsx
+++ grupo3-frontend-react/grupo3-react-application/src/views/login/user/login_user.jsx
@@ -1,85 +1,85 @@
-import React, { Component } from "react"
-import { Form, Button, FormGroup, FormControl, Alert } from "react-bootstrap"
-import CSSModules from 'react-css-modules'
-import axios from 'axios'
-import styles from "./login_user.css"
+import React, { Component } from "react";
+import { Form, Button, FormGroup, FormControl, Alert } from "react-bootstrap";
+import CSSModules from "react-css-modules";
+import axios from "axios";
+import styles from "./login_user.css";
 
 class LoginUser extends Component {
-    constructor() {
-        super()
-        this.state = {
-            email: "",
-            password: ""
-        }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.username.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      let response = await axios.post("http://localhost:8080/login", {
+        username: this.state.username,
+        password: this.state.password
+      });
+
+      alert(response);
+    } catch (error) {
+      alert(error);
     }
+  };
 
-    validateForm() {
-        return (this.state.email.length > 0 && this.state.password.length > 0)
-    }
+  render() {
+    return (
+      <div className="Login">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label>Username/Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+              placeholder="Enter username or email"
+            />
+          </Form.Group>
 
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        })
-    }
-
-    handleSubmit = async event => {
-        event.preventDefault();
-        try{
-            let response = await axios.post(
-                'http://localhost:8080/login',
-                {
-                    username: this.state.email,
-                    password: this.state.password
-                }                
-            );
-
-            alert(response);
-        }
-        catch(error) {
-            alert(error);
-        }
-    }
-
-    render() {
-        return (
-            <div className="Login">
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="email">
-                        <Form.Label>Username/Email</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            placeholder="Enter username or email"
-                        />
-                    </Form.Group>
-
-                    <Form.Group controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="signup">
-                        <Form.Label>
-                            Don't have an account? <Alert.Link href="http://localhost:8008/signup">Sign Up</Alert.Link>
-                        </Form.Label>
-                    </Form.Group>
-                    <Button
-                        block
-                        disabled={!this.validateForm()}
-                        type="submit"
-                        variant="primary">
-                        Login
-                    </Button>
-                </Form>
-            </div>
-        )
-    }
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group controlId="signup">
+            <Form.Label>
+              Don't have an account?{" "}
+              <Alert.Link href="http://localhost:8008/signup">
+                Sign Up
+              </Alert.Link>
+            </Form.Label>
+          </Form.Group>
+          <Button
+            block
+            disabled={!this.validateForm()}
+            type="submit"
+            variant="primary"
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
-export default CSSModules(LoginUser, styles, {allowMultiple: true})
+export default CSSModules(LoginUser, styles, { allowMultiple: true });
