bf1bc11ab479c49743dcfb74e59892a54bfe7272
leoAristarco <leoceit@gmail.com>
2019-05-26 00:05:23 -0300

    cambio el nombre de los endpoint del administrator

--- grupo3-backend/grupo3-spring-controller/src/main/java/utn/tacs/grupo3/spring/controller/impl/AdministratorControllerImpl.java
+++ grupo3-backend/grupo3-spring-controller/src/main/java/utn/tacs/grupo3/spring/controller/impl/AdministratorControllerImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import utn.tacs.grupo3.model.ListOfPlaces;
 import utn.tacs.grupo3.model.Place;
+import utn.tacs.grupo3.model.User;
 import utn.tacs.grupo3.model.exception.ExceptionbyResourceNotFound;
 import utn.tacs.grupo3.repository.PlaceRepository;
 import utn.tacs.grupo3.repository.UserRepository;
@@ -14,7 +15,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/places")
+@RequestMapping("/administrator")
 public class AdministratorControllerImpl implements AdministratorController {
 
     @Autowired
@@ -23,7 +24,13 @@ public class AdministratorControllerImpl implements AdministratorController {
     private PlaceRepository placeRepository;
 
     @Override
-    @GetMapping("/places-in-common")
+    @GetMapping("users/{user-id}")
+    public User userById(@PathVariable("user-id") String userId) throws ExceptionbyResourceNotFound {
+        return userRepository.userByUsername(userId);
+    }
+
+    @Override
+    @GetMapping("places/places-in-common")
     public Map<String, Boolean> placesInCommon(@RequestParam("list-id-1") int listId1, @RequestParam("list-id-2") int listId2) throws ExceptionbyResourceNotFound {
         ListOfPlaces listOfPlaces1 = userRepository.listOfPlacesById(listId1);
         ListOfPlaces listOfPlaces2 = userRepository.listOfPlacesById(listId2);
@@ -31,14 +38,14 @@ public class AdministratorControllerImpl implements AdministratorController {
     }
 
     @Override
-    @GetMapping("/{place-id}/interested-users")
+    @GetMapping("places/{place-id}/interested-users")
     public Map<String, Long> numberOfInterestedUsers(@PathVariable("place-id") String placeId) throws ExceptionbyResourceNotFound {
         Place place = placeRepository.placeByName(placeId);
         return Collections.singletonMap("totalOfUsersInterested", userRepository.amountOfUsersInterestedIn(place));
     }
 
     @Override
-    @GetMapping("/registered-places")
+    @GetMapping("places/registered-places")
     public Map<String, Long> registeredPlaces(@RequestParam("days") int days) {
         placeRepository.setCurrentDate(LocalDate.now());
         return Collections.singletonMap("totalRegisteredPlaces", placeRepository.amountOfPlacesRegisteredInTheSystemInTheLast(days));
