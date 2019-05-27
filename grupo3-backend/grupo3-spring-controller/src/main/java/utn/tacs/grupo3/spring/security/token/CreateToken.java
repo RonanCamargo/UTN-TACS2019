cc0d79c91cd243e9bb40fd70564d3162bbda1b8c
leoAristarco <leoceit@gmail.com>
2019-05-27 02:20:26 -0300

    Agrego token a la respuesta como un json. #58

--- grupo3-backend/grupo3-spring-controller/src/main/java/utn/tacs/grupo3/spring/security/token/CreateToken.java
+++ grupo3-backend/grupo3-spring-controller/src/main/java/utn/tacs/grupo3/spring/security/token/CreateToken.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 public class CreateToken {
@@ -17,14 +18,33 @@ public class CreateToken {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
 
-        String token = Jwts.builder()
+        String token = createToken(authentication, authorities);
+
+        //agregar al encabezado el token
+        addTheTokenToTheHeader(res, token);
+
+        sendTokenAsJson(res,token);
+    }
+
+    private void addTheTokenToTheHeader(HttpServletResponse res, String token) {
+        res.addHeader("Authorization", "Bearer " + token);
+    }
+
+    private String createToken(Authentication authentication, String authorities) {
+        return Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim("ROL", authorities)
                 // Hash con el que firmaremos la clave
                 .signWith(SignatureAlgorithm.HS512, "P@tit0")
                 .compact();
+    }
 
-        //agregamos al encabezado el token
-        res.addHeader("Authorization", "Bearer " + token);
+    private void sendTokenAsJson(HttpServletResponse res, String token) {
+        res.setContentType("application/json");
+        try {
+            res.getOutputStream().print("{\"token\":\""+token+"\"}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
