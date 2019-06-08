5105ca1d3e577bfc970ee0e9e3f0aabfecee5d05
Ronan <ronan.cam@gmail.com>
2019-06-08 03:01:11 -0300

    Agrego dependencias

---
+++ grupo3-backend/grupo3-service/src/main/java/utn/tacs/grupo3/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package utn.tacs.grupo3.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import utn.tacs.grupo3.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
