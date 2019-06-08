bb7ae1e652993aa1030404fb4b645edbc76a13b0
Ronan <ronan.cam@gmail.com>
2019-06-08 03:51:28 -0300

    Implementacion de ListOfPlacesController con mongo, sin excepciones

--- grupo3-backend/grupo3-spring-controller/src/main/java/utn/tacs/grupo3/spring/controller/impl/ListOfPlacesControllerImpl.java
+++ grupo3-backend/grupo3-spring-controller/src/main/java/utn/tacs/grupo3/spring/controller/impl/ListOfPlacesControllerImpl.java
@@ -14,43 +14,51 @@ import org.springframework.web.bind.annotation.RestController;
 
 import utn.tacs.grupo3.model.ListOfPlaces;
 import utn.tacs.grupo3.model.exception.ExceptionbyResourceNotFound;
-import utn.tacs.grupo3.repository.UserRepository;
+import utn.tacs.grupo3.repository.mongo.UserRepository;
 import utn.tacs.grupo3.spring.controller.ListOfPlacesController;
 
 @RestController
 @RequestMapping("/users")
 public class ListOfPlacesControllerImpl implements ListOfPlacesController {
+    //TODO sacar repo en memoria
+    private utn.tacs.grupo3.repository.UserRepository userRepository;
+    
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepoMongo;
 
     @Override
     @GetMapping("/{user-id}/list-of-places")
     public List<ListOfPlaces> listsOfListOfPlaces(@PathVariable("user-id") String userId) throws ExceptionbyResourceNotFound {
-        return userRepository.userByUsername(userId).getListsOfPlaces();
+        return userRepoMongo.userByUsername(userId).getListsOfPlaces();
+//        		.userByUsername(userId).getListsOfPlaces();
     }
 
     @Override
     @PostMapping("/{user-id}/list-of-places/{list-id}")
     public void createListOfPlaces(@PathVariable("user-id") String userId, @PathVariable("list-id") String listId) throws ExceptionbyResourceNotFound {
-        userRepository.userByUsername(userId).createListOfPlaces(listId);
+//        userRepository.userByUsername(userId).createListOfPlaces(listId);
+    	userRepoMongo.createListOfPlaces(userId, listId);
     }
 
     @Override
     @GetMapping("/{user-id}/list-of-places/{list-id}")
     public ListOfPlaces listOfPlacesListById(@PathVariable("user-id") String userId, @PathVariable("list-id") String listId) throws ExceptionbyResourceNotFound {
-        return userRepository.userByUsername(userId).listOfPlacesByName(listId);
+//        return userRepository.userByUsername(userId).listOfPlacesByName(listId);
+    	return userRepoMongo.findListOfPlaces(userId, listId);
     }
 
     @Override
     @DeleteMapping("/{user-id}/list-of-places/{list-id}")
     public void deleteListOfPlacesList(@PathVariable("user-id") String userId, @PathVariable("list-id") String listId) throws ExceptionbyResourceNotFound {
-        userRepository.userByUsername(userId).removeFromListsOfPlaces(userRepository.userByUsername(userId).listOfPlacesByName(listId));
+//        userRepository.userByUsername(userId).removeFromListsOfPlaces(userRepository.userByUsername(userId).listOfPlacesByName(listId));
+    	userRepoMongo.deleteListOfPlaces(userId, listId);
     }
 
     @Override
     @PutMapping("/{user-id}/list-of-places/{list-id}")
     public void editListOfPlacesList(@PathVariable("user-id") String userId, @PathVariable("list-id") String listId, @RequestParam("new-name") String newName) throws ExceptionbyResourceNotFound {
-        userRepository.userByUsername(userId).listOfPlacesByName(listId).setListName(newName);
+//        userRepository.userByUsername(userId).listOfPlacesByName(listId).setListName(newName);
+    	userRepoMongo.renameListOfPlaces(userId, listId, newName);
     }
 
 }
