cf4ac1ad4d9eab37008f7e0659f7919478ab42fc
Ronan <ronan.cam@gmail.com>
2019-06-08 02:52:02 -0300

    Agrego modulo Service

---
+++ grupo3-backend/grupo3-service/src/main/java/utn/tacs/grupo3/service/App.java
@@ -0,0 +1,13 @@
+package utn.tacs.grupo3.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
